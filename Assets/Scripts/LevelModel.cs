fd3584ce957a52c63f9ce114808f3c67e82dc5ff
Dallas Kelle <dkelle@Dallass-MBP.attlocal.net>
2015-01-02 13:32:11 -0600

    Added correct scale and color to trail

--- Assets/Scripts/LevelModel.cs
+++ Assets/Scripts/LevelModel.cs
@@ -24,17 +24,22 @@ public class LevelModel : MonoBehaviour {
 			//Slot 0 should be the current transform of the character
 			for (int i = 0; i < trail.Length - 1; i++) {
 					if (trail [i + 1].GetComponent<SpriteRenderer> ().sprite != null) {
-							trail [i].GetComponent<SpriteRenderer> ().sprite = (Sprite)(Sprite.Instantiate (trail [i + 1].GetComponent<SpriteRenderer> ().sprite));
-							trail [i].GetComponent<SpriteRenderer> ().transform.position = trail [i + 1].transform.position;
+						SpriteRenderer r = trail [i + 1].GetComponent<SpriteRenderer> ();
+						trail [i].GetComponent<SpriteRenderer> ().sprite = trail[i+1].GetComponent<SpriteRenderer> ().sprite;
+						trail [i].GetComponent<SpriteRenderer> ().transform.position = trail[i+1].transform.position;
+						trail [i].GetComponent<SpriteRenderer> ().transform.localScale = trail[i+1].transform.localScale;
+						trail [i].GetComponent<SpriteRenderer> ().color = new Color(r.color.r, r.color.g, r.color.b, r.color.a - .1f);
+						
 					} else
 							Debug.Log ("Trail is null");
-					Debug.Log ("X of " + i + " sprite: " + trail [i].GetComponent<SpriteRenderer> ().transform.position.x);
 			}
 
 
 
 			trail [9].GetComponent<SpriteRenderer> ().sprite = (Sprite)(Sprite.Instantiate (character.sprite));
 			trail [9].GetComponent<SpriteRenderer> ().transform.position = character.transform.position;
+			trail [9].GetComponent<SpriteRenderer> ().transform.localScale = character.transform.localScale;
+			trail [9].GetComponent<SpriteRenderer> ().color = new Color(character.color.r, character.color.g, character.color.b, character.color.a - .1f);
 			Debug.Log ("X of oldest sprite: " + trail [0].GetComponent<SpriteRenderer> ().transform.position.x);
 			
 			
@@ -44,4 +49,5 @@ public class LevelModel : MonoBehaviour {
 		}
 
 	}
+
 }
