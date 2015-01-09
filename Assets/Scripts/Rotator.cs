cdcbfe3308fc512ba7ec36def8f701c64ecc5ebd
Dallas Kelle <dkelle@Dallass-MBP.attlocal.net>
2015-01-09 14:38:13 -0600

    The rotator can now take different speeds and directions

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -3,17 +3,30 @@ using System.Collections;
 
 public class Rotator : MonoBehaviour {
 
+	public Vector3 axis = Vector3.up;
+	public float speed = 5;
+	
 	// Use this for initialization
 	void Start () {
+
 		transform.localRotation = new Quaternion (0, 0, 0, 0);
 		StartCoroutine(TimedUpdate());
+	}
+
+	public void SetSpeed(float speed){
+		Debug.Log ("setting spoeed");
+		this.speed = speed;
+	}
+
+	public void SetAxis(Vector3 axis){
+		this.axis = axis;
 	}
 	
 	// Update is called once per .25 seconds
 	IEnumerator TimedUpdate () {
 		while (true) {
-			transform.Rotate(Vector3.up * 5);
-			
+			transform.Rotate(axis * speed);
+
 			//Wait x seconds before we preform another update
 			yield return new WaitForSeconds (.025f);
 		}
